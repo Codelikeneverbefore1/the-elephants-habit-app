72f87fe26e85abe93ce46cdf4f7a1269296176bf
ombymo <ombymo@gmail.com>
2023-07-20 12:59:28 +0200

    first commit/first check

--- habapp-backend/server/index.js
+++ habapp-backend/server/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
-
+const dotenv = require('dotenv');
+const connectDB = require('./config/database')
 const PORT = process.env.PORT || 3001;
+const colors = require('colors')
+
+connectDB()
 
 const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}))
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello world! (from server)" });

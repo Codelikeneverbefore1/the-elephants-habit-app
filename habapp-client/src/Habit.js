5bc353c6e37b353895ab2ebe3b90d5ea3e30b209
Lasse Pedersen <133651210+Codelikeneverbefore1@users.noreply.github.com>
2023-07-22 11:32:59 +0200

    Add files via upload

--- habapp-client/src/Habit.js
+++ habapp-client/src/Habit.js
@@ -1,30 +1,77 @@
-import React from "react"
+import React, { useState } from 'react';
 
-const Habit = () => {
-    return (
-        <div>
-            <h2>New Habit</h2>
-            <form>
-                <label>Habit Name</label>
-                <input
-                    type="text"
-                    required
-                />
-                <label>Habit Goal</label>
-                <input
-                    type="text"
-                    required
-                />    
-                <label>Habit Duration</label>
-                <input
-                    type="text"
-                    required
-                />
-                <button>Submit Habit</button>        
-            </form>   
+function Habit() {
+  const [list, setList] = useState([]);
+  const [name, setName] = useState('');
+  const [goal, setGoal] = useState('');
+  const [deadline, setDeadline] = useState('');
 
-        </div>
-    )
+  const addHabit = () => {
+    // Check if all required fields are filled
+    if (!name || !goal || !deadline) {
+      alert('Please fill out all required fields.');
+      return;
+    }
+
+    const newHabit = {
+      id: Math.random(),
+      habit: name,
+      goal: goal,
+      deadline: deadline,
+    };
+
+    // Add the Habit to the list
+    setList([...list, newHabit]);
+
+    // Clear input boxes
+    setName('');
+    setGoal('');
+    setDeadline('');
+  };
+
+  return (
+    <div>
+      <h2>Create a Habit</h2>
+
+      <label>Name</label>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required 
+      />
+
+      <label>Goal</label>
+      <input
+        type="text"
+        value={goal}
+        onChange={(e) => setGoal(e.target.value)}
+        required 
+      />
+
+      <label>Duration</label>
+      <input
+        type="date"
+        value={deadline}
+        onChange={(e) => {
+          const date = new Date(e.target.value);
+          const formattedDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+          setDeadline(formattedDate);
+        }}
+        required 
+      />
+
+      <button onClick={addHabit}>Submit Habit</button>
+
+      <ul>
+        {list.map((habit) => (
+          <li key={habit.id}>
+            <strong>{habit.habit}</strong> - {habit.goal} - By {habit.deadline}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Habit
+export default Habit;

b3b3cb27c9585e0b4a4f238bdf1a14d0e76276e9
ombymo <ombymo@gmail.com>
2023-09-16 14:46:34 +0200

    Emergency clean up, Login/register removed

--- habapp-client/src/index.js
+++ habapp-client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -11,9 +11,7 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
